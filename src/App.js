c589d208d4a82983d5d978de5a72cad3daf9b99c
pluto1019 <pluto1019@naver.com>
2021-08-02 18:10:37 +0900

    add useInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div className='App'></div>;
-}
+import { useInput } from './libs/jooks';
+
+const UseJooks = ({ title, children }) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      {children}
+    </>
+  );
+};
+
+const UseInput = () => {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput('Mr.', maxLen);
+  return <input placeholder='name' {...name} />;
+};
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Jooks example</h1>
+
+      <UseJooks title='useInput'>
+        <UseInput />
+      </UseJooks>
+    </div>
+  );
+};
 
 export default App;
